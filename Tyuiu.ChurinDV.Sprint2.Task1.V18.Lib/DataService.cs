50b0365a18f73042409389cf550e15b40b48f4b3
ChurinDmitriy <mister.dim2017111@gmail.com>
2023-10-04 23:39:05 +0500

    Task1

---
+++ Tyuiu.ChurinDV.Sprint2.Task1.V18.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+//int a = 155;
+//int b = 696;
+//int c = 242;
+//int d = 541;
+
+
+namespace Tyuiu.ChurinDV.Sprint2.Task1.V18.Lib
+{
+    public class DataService : ISprint2Task1V18
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (b > a) | (a == c);
+            res[1] = (b > c) & (a < d);
+            res[2] = (a != d) || (d > b);
+            res[3] = (c >= a) && (b == d);
+            res[4] = !(res[3]);
+            res[5] = (a != d) ^ (c <= b);
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ChurinDV.Sprint2.Task1.V18/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ChurinDV.Sprint2.Task1.V18.Lib;
+
+namespace Tyuiu.ChurinDV.Sprint2.Task1.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int a = 155;
+            int b = 696;
+            int c = 242;
+            int d = 541;
+
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Чурин Дмитрий Владимирович | ИСТНб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=)         *");
+            Console.WriteLine("* и логических операций (|, &, ||, &&, !, ^)                              *");
+            Console.WriteLine("* а также арифметических выражений, которая вернет                        *");
+            Console.WriteLine("* логическую последовательность(массив):                                  *");
+            Console.WriteLine("* (True, True, True, False, True, False)                                  *");
+            Console.WriteLine("* при a = 155, b = 696, c = 242, d = 541                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}

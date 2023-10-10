17dc817af6b3d6446ca4323b747c8687acbcc7b7
ChurinDmitriy <mister.dim2017111@gmail.com>
2023-10-10 23:47:04 +0500

    Task6

---
+++ Tyuiu.ChurinDV.Sprint2.Task6.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChurinDV.Sprint2.Task6.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChurinDV.Sprint2.Task6.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e19e6d64-a466-4541-b1c6-033e822e63e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ChurinDV.Sprint2.Task6.V3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ChurinDV.Sprint2.Task6.V3.Lib;
+
+
+namespace Tyuiu.ChurinDV.Sprint2.Task6.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Чурин Дмитрий Владимирович | ИСТНб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+            Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат.   *");
+            Console.WriteLine("* Условие:Составить программу,которая в зависимости от порядкового номера *");
+            Console.WriteLine("* дня недели (1, 2, …, 7) выводит на экран его название                   *");
+            Console.WriteLine("* (понедельник, вторник, …, воскресенье).                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите порядковый номер дня недели: ");
+            int numMouth = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numMouth < 1) || (numMouth > 7))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "Это день недели: " + ds.FindDayName(numMouth);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
